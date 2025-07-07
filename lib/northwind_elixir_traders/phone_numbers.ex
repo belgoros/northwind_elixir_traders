a247864bf79698c688b5bcb5913a2b381191d83a
belgoros <s.cambour@gmail.com>
2025-07-07 18:13:19 +0200

    Add PhoneNumbers.make_readable/1

--- lib/northwind_elixir_traders/phone_numbers.ex
+++ lib/northwind_elixir_traders/phone_numbers.ex
@@ -5,4 +5,15 @@ defmodule NorthwindElixirTraders.PhoneNumbers do
   def is_intl?(phone) when is_bitstring(phone), do: Regex.match?(@intl_regex, phone)
 
   def is_nanp?(phone) when is_bitstring(phone), do: Regex.match?(@nanp_regex, phone)
+
+  def make_readable(phone) when is_bitstring(phone) do
+    phone
+    |> then(&Regex.replace(~r/[^\d\s]/, &1, " "))
+    |> then(&Regex.replace(~r/\s+/, &1, " "))
+    |> String.trim_leading()
+  end
+
+  def compose_intl(dialcode, readable) when is_bitstring(dialcode) and is_bitstring(readable) do
+    to_string(["+", dialcode, " ", readable])
+  end
 end
