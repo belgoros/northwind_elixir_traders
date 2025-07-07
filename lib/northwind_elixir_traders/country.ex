846b39eea27c6a23d3a8ef106567b2dd1f0ed8c8
belgoros <s.cambour@gmail.com>
2025-07-07 17:06:42 +0200

    Refactor and move country relted functions to Country module

--- lib/northwind_elixir_traders/country.ex
+++ lib/northwind_elixir_traders/country.ex
@@ -6,6 +6,7 @@ defmodule NorthwindElixirTraders.Country do
 
   @name_mxlen 50
   @dial_mxlen 14
+  @url "https://raw.githubusercontent.com/datasets/country-codes/2ed03b6993e817845c504ce9626d519376c8acaa/data/country-codes.csv"
 
   schema "countries" do
     field(:name, :string)
@@ -33,6 +34,52 @@ defmodule NorthwindElixirTraders.Country do
   def get_dial_by(field, value) when is_atom(field) and is_bitstring(value) do
     criterion = Keyword.new([{field, value}])
     Repo.one(from(c in __MODULE__, where: ^criterion, select: c.dial))
+  end
+
+  def import do
+    get_csv_rows() |> process_csv() |> Enum.map(&csv_tuple_to_record/1)
+  end
+
+  defp csv_tuple_to_record({_name, _dial, _alpha3} = country) do
+    [:name, :dial, :alpha3]
+    |> Enum.zip(Tuple.to_list(country))
+    |> Map.new()
+    |> then(&changeset(struct(__MODULE__), &1))
+    |> Repo.insert()
+  end
+
+  defp get_csv_rows() do
+    {status, result} = :httpc.request(@url)
+
+    case {status, result} do
+      {:ok, {_status, _headers, body}} ->
+        {:ok,
+         body
+         |> List.to_string()
+         |> String.split("\n")
+         |> Enum.map(&fix_csv_row/1)
+         |> Enum.map(&String.split(&1, ","))}
+
+      {:error, {status, _, _}} ->
+        {:error, status}
+    end
+  end
+
+  defp fix_csv_row(row) when is_bitstring(row) do
+    regex = ~r/"([^"]*)"/
+    String.replace(row, regex, &String.replace(&1, ",", "|")) |> String.replace("\"", "")
+  end
+
+  defp process_csv({:ok, [headings | data]}) do
+    indices =
+      ["CLDR display name", "Dial", "ISO3166-1-Alpha-3"]
+      |> Enum.map(fn colname -> Enum.find_index(headings, &(&1 == colname)) end)
+
+    data
+    |> Enum.map(fn row -> Enum.map(indices, &Enum.at(row, &1)) end)
+    |> Enum.filter(fn r -> "" not in r and nil not in r end)
+    |> Enum.map(&List.to_tuple(&1))
+    |> Enum.map(fn {country, dial, iso} -> {country, String.replace(dial, "-", ""), iso} end)
   end
 
   def changeset(category, params \\ %{}) do
