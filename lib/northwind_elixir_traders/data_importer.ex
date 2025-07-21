ebb9c07180c883979cd270fda59327e8419528d7
belgoros <s.cambour@gmail.com>
2025-07-21 16:30:58 +0200

    Refactor prioritize/0

--- lib/northwind_elixir_traders/data_importer.ex
+++ lib/northwind_elixir_traders/data_importer.ex
@@ -250,11 +250,23 @@ defmodule NorthwindElixirTraders.DataImporter do
   end
 
   def prioritize() do
-    make_dependency_map()
-    |> gather()
-    |> Enum.map(fn {k, v} -> {k, List.flatten([Map.values(v) | Map.keys(v)])} end)
-    |> Enum.sort_by(fn {_, dependencies} -> length(dependencies) end)
-    |> Enum.map(fn {k, _v} -> k end)
+    edges = make_erd_graph()
+    unvisited = (Map.keys(edges) ++ Map.values(edges)) |> Enum.uniq() |> Enum.shuffle()
+    sorted = []
+
+    results =
+      unvisited
+      |> Enum.reduce_while({sorted, unvisited}, fn n, acc ->
+        if Enum.empty?(unvisited) do
+          {:halt, acc}
+        else
+          {sorted, unvisited} = acc
+          {:cont, visit(n, edges, sorted, unvisited)}
+        end
+      end)
+
+    # return the sorted list
+    elem(results, 0)
   end
 
   def teardown() do
