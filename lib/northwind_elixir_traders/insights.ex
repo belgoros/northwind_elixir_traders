9ac067513b967abba86caca69ac16fe01038f3cf
belgoros <s.cambour@gmail.com>
2025-07-23 23:02:25 +0200

    Create insights.ex

---
+++ lib/northwind_elixir_traders/insights.ex
@@ -0,0 +1,44 @@
+defmodule NorthwindElixirTraders.Insights do
+  import Ecto.Query
+  alias NorthwindElixirTraders.{Repo, Product, Order, OrderDetail}
+  @max_concurrency 2
+  @timeout 5
+
+  def query_order_details_by_order(order_id) do
+    OrderDetail
+    |> join(:inner, [od], p in Product, on: od.product_id == p.id)
+    |> where([od], od.order_id == ^order_id)
+  end
+
+  def query_order_detail_values(order_id) do
+    query_order_details_by_order(order_id)
+    |> select([od, p], od.quantity * p.price)
+  end
+
+  def query_order_total_value(order_id) do
+    query_order_details_by_order(order_id)
+    |> select([od, p], sum(od.quantity * p.price))
+  end
+
+  def calculate_order_value(%Order{id: order_id}), do: calculate_order_value(order_id)
+
+  def calculate_order_value(order_id) when not is_map(order_id) do
+    order_id |> query_order_total_value() |> Repo.one()
+  end
+
+  def calculate_total_value_of_orders(orders, opts \\ [max_concurrency: @max_concurrency])
+      when is_list(orders) and is_list(opts) do
+    mc =
+      if Keyword.has_key?(opts, :max_concurrency),
+        do: Keyword.get(opts, :max_concurrency),
+        else: @max_concurrency
+
+    Task.async_stream(orders, &calculate_order_value/1,
+      ordered: false,
+      timeout: @timeout,
+      max_concurrency: mc
+    )
+    |> Enum.to_list()
+    |> Enum.reduce(0.0, fn {_status, value}, acc -> acc + value end)
+  end
+end
