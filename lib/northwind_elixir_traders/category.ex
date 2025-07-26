fb267165157f71d8c4f633f20abcbe77b69aed8d
belgoros <s.cambour@gmail.com>
2025-07-26 23:43:49 +0200

    Add has_many with orders relation

--- lib/northwind_elixir_traders/category.ex
+++ lib/northwind_elixir_traders/category.ex
@@ -10,6 +10,7 @@ defmodule NorthwindElixirTraders.Category do
     field(:name, :string)
     field(:description, :string)
     has_many(:products, Product)
+    has_many(:orders, through: [:products, :order_details, :order])
     timestamps(type: :utc_datetime)
   end
 
